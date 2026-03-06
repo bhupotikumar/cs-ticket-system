2302d075073002aa62f6050d4c52da183918113d
bhupotikumar <bhupotikumar123@gmail.com>
2026-03-06 20:10:37 +0600

    Loading updated

--- src/components/Tickets/TIckets.jsx
+++ src/components/Tickets/TIckets.jsx
@@ -1,33 +1,44 @@
-import { Suspense, useEffect } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import TicketCard from './TicketCard';
 import Loading from '../Loading/Loading';
 
 const Tickets = ({ handleTicket, tickets, setTickets }) => {
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchTickets = async () => {
-            const res = await fetch('./tickets.json');
-            const data = await res.json();
-            setTickets(data);
+            setLoading(true);
+            try {
+                const res = await fetch('./tickets.json');
+                const data = await res.json();
+                setTickets(data);
+            } catch (err) {
+                console.log("Failed to fetch data", err);
+            } finally {
+                setLoading(false);
+            }
+
         };
         fetchTickets();
     }, [setTickets]);
+
+    if (loading) return <Loading />;
+
+    if (!tickets.length) return <p className="text-center mt-4">No tickets available</p>;
 
 
     return (
         <div>
             <h1 className='text-2xl text-center md:text-left font-semibold'>Customer Tickets</h1>
-            <Suspense fallback={<Loading />}>
-                <div className="mt-2 grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
-                    {tickets.map(ticket => {
-                        return <TicketCard
-                            key={ticket.id}
-                            ticket={ticket}
-                            onClick={handleTicket}
-                        />
-                    })}
-                </div>
-            </Suspense>
+            <div className="mt-2 grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+                {tickets.map(ticket => {
+                    return <TicketCard
+                        key={ticket.id}
+                        ticket={ticket}
+                        onClick={handleTicket}
+                    />
+                })}
+            </div>
         </div>
     );
 };
